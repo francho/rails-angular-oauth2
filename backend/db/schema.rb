242781ff4c71a9318fff272e8301556b67392718
Rafa García <contacto@rafagarcia.net>
2014-11-11 16:03:57 +0100

    Omniauth migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110150120) do
+ActiveRecord::Schema.define(version: 20141111145622) do
 
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 20141110150120) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
